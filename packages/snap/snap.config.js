108ae32f4b6a85cf5534afb5993dae6783dbd03c
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-03-29 15:17:08 +0530

    chore: add env support

--- packages/snap/snap.config.js
+++ packages/snap/snap.config.js
@@ -1,6 +1,17 @@
+const envify = require('envify/custom');
+require('dotenv').config();
+
 module.exports = {
   cliOptions: {
     src: './src/index.ts',
     port: 8080,
+  },
+  bundlerCustomizer: (bundler) => {
+    bundler.transform(
+      envify({
+        ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY,
+        ETHERSCAN_API_KEY: process.env.ETHERSCAN_API_KEY,
+      }),
+    );
   },
 };

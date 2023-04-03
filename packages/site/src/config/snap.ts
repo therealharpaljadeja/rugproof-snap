f00bcc3046c59d3db850e89026c90140ac126af3
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-04-03 19:25:18 +0530

    fix: vercel gatsby env

--- packages/site/src/config/snap.ts
+++ packages/site/src/config/snap.ts
@@ -2,7 +2,7 @@
  * The snap origin to use.
  * Will default to the local hosted snap if no value is provided in environment.
  */
-console.log(process.env.REACT_APP_SNAP_ORIGIN);
-export const defaultSnapOrigin = process.env.REACT_APP_SNAP_ORIGIN
-  ? process.env.REACT_APP_SNAP_ORIGIN
-  : `local:http://localhost:8080`;
+export const defaultSnapOrigin =
+  process.env.GATSBY_VERCEL_ENV === 'production'
+    ? 'npm:rugproof-snap'
+    : `local:http://localhost:8080`;

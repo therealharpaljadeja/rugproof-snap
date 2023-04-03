464ac00123fb19c687850ab2b26448b76756101d
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-04-03 18:01:38 +0530

    chore: vercel env problem

--- packages/site/src/config/snap.ts
+++ packages/site/src/config/snap.ts
@@ -2,7 +2,7 @@
  * The snap origin to use.
  * Will default to the local hosted snap if no value is provided in environment.
  */
-console.log(process.env.SNAP_ORIGIN);
-export const defaultSnapOrigin = process.env.SNAP_ORIGIN
-  ? process.env.SNAP_ORIGIN
+console.log(process.env.REACT_APP_SNAP_ORIGIN);
+export const defaultSnapOrigin = process.env.REACT_APP_SNAP_ORIGIN
+  ? process.env.REACT_APP_SNAP_ORIGIN
   : `local:http://localhost:8080`;
